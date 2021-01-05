e56d5c395004cab101db8c4d3480baffa4db4d9a
xela redna <xela@xelas-MacBook-Air.local>
2021-01-05 16:32:06 -0500

    added under firebase

--- NeighborhoodApp/NeighborhoodApp/AppDelegate.swift
+++ NeighborhoodApp/NeighborhoodApp/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //I was here
         FirebaseApp.configure()  //sets up firebase
         return true
     }
